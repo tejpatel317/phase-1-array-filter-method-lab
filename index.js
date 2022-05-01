b99fdeecb4985481b9e031cafd1e27d410734c26
Tej Patel <tejpatel317@gmail.com>
2022-05-01 16:31:49 -0400

    commit tree dbaf4f83e115470fcea6c65f6ee5b29246af870f

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching(arrayOfDriverNames, string) {
+    const x = arrayOfDriverNames.filter(function(user) {
+        return (user.toLowerCase() === string.toLowerCase());
+    })
+    return x;
+}
+
+function fuzzyMatch(arrayOfDriverNames, string) {
+    const x = arrayOfDriverNames.filter(function(user) {
+        return (user.charAt(0) === string.charAt(0))
+    })
+    return x;
+}
+
+function matchName(arrayOfDriverObject, string) {
+    const x = arrayOfDriverObject.filter(function(user) {
+        return (user.name === string)
+    })
+    return x;
+}
